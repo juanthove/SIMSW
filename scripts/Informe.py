1043175b7e91bb03c766fb03adccf7183938a788
mauro <Mauromesa2020@gmail.com>
2026-05-16 23:06:49 -0300

    Agrego tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf local. La idea es descargarse un modelo, ponerlo dentro de una carpeta model y llamarlo desde ahi. La configuracion se hace desde la linea 36 de informe.py. El nombre que le puse a la variable es tinyllamaQ4-local, cambiar en caso de usar otro modelo. IMPORTANTE: El archivo debe ser .gguf

--- scripts/Informe.py
+++ scripts/Informe.py
@@ -4,13 +4,14 @@ import traceback
 from groq import Groq #pip install groq
 from langchain_google_genai import ChatGoogleGenerativeAI
 import ollama #pip install ollama
-
+from pathlib import Path
+from llama_cpp import Llama #pip install llama-cpp-python
 
 
 class Informe():
     def __init__(self):
         load_dotenv()
-
+        self.__llm = None
         self.__model = os.getenv("MODEL")
         if not self.__model:
             raise ValueError("No se encontró el modelo de LLM en el entorno.")
@@ -27,6 +28,33 @@ class Informe():
             if not self.__password:
                     raise ValueError("No se encontró la clave para el modelo llama-3.3-70b-versatile en el entorno.")
         
+        elif(self.__model == "tinyllamaQ4-local"):
+            print("Voy a buscarlo")
+            model_path = (
+                Path(__file__).resolve().parent.parent
+                / "models"
+                / "tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf"
+            )
+
+            if not model_path.exists():
+                raise ValueError(
+                    f"No existe el modelo GGUF: {model_path}"
+                )
+
+            try:
+                self.__llm = Llama(
+                    model_path=str(model_path),
+                    n_ctx=2048,
+                    verbose=False
+                )
+
+            except Exception as e:
+                raise ValueError(
+                    f"Error cargando modelo GGUF: {str(e)}"
+                )
+
+            self.__password = ""
+
         elif (self.__model == "tinyllama:latest"):
             self.__password = ""
 
@@ -60,6 +88,24 @@ class Informe():
                     temperature=0
                 )
                 return response.choices[0].message.content or ""
+            
+            elif(self.__model == "tinyllamaQ4-local"):
+                print("Entrando en preguntar")
+                response = self.__llm.create_chat_completion(
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": promptCausa
+                        }
+                    ],
+                    temperature=0
+                )
+                print("Voy a retornar")
+                return (
+                    response["choices"][0]
+                    ["message"]["content"]
+                )
+
             elif(self.__model == "tinyllama:latest"):
                 try:
                     modelos = ollama.list()
